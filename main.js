f9284519e44a111814756b1cec1db60a6f3467f9
Nathan Klundt <nathan@blockchainjedi.com>
2023-08-15 16:07:57 -0600

    fix cron; write log to file

--- main.js
+++ main.js
@@ -3,21 +3,17 @@ const dayjs = require('dayjs');
 const CronJob = require('cron').CronJob;
 const activate_roku = require('./activate_roku');
 
-// This process will run every 15 minutes
-const every_15_min = '15 * * * *';
-const every_30_sec = '30 * * * * *';
+const every_15_min = '*/15 * * * *';
+const every_10_sec = '*/10 * * * * *';
 const job = new CronJob(
-  every_30_sec,
+  every_10_sec,
   look_for_checkin,
   null,
   true,
 );
 
-function test_function () {
-  console.log(dayjs());
-}
-
 function look_for_checkin () {
+  console.log('running look_for_checkin');
   const data = JSON.parse(
     fs.readFileSync('../mockdata.json'));
   const { checkin_time
@@ -25,25 +21,32 @@ function look_for_checkin () {
          ,listing: {property_id}
          ,guest } = data;
 
-  console.log(data);
   const checkin_time_d = dayjs(checkin_time);
   const now = dayjs();
 
   const diff = checkin_time_d.diff(now, 'hour', true);
+  console.log(`hours until checking: ${diff}`);
   if (diff < 1.0 && diff >= 0.0) {
     // checkin time is within 1 hour, 
     // read checkin_activated.json
-    const history = JSON.parse(
-      fs.readFileSync('history.json'));
+    if (!fs.existsSync('history.json')) {
+      fs.writeFileSync('history.json', JSON.stringify({}));
+    }
+    const history = JSON.parse(fs.readFileSync('history.json'));
     if (history.user_id == user_id
        && history.property_id == property_id
        && history.checkin_time == checkin_time) {
       // roku already activated
-      console.log('roku already activated');
+      fs.appendFile('log.txt', `cron ran at ${dayjs().toJSON()}; roku already activated\n`, (err) => {
+        if (err) throw err;
+      });
       return;
     }
     activate_roku(guest);
     write_history(user_id, property_id, checkin_time);
+  }
+  else {
+    console.log('outside of time window');
   }
 }
 
--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const fs = require('fs');
 const dayjs = require('dayjs');
 
-data = fs.readFileSync('../mockdata.json');
+data = fs.readFileSync('../mockdata.json')
 json = JSON.parse(data);
-const date = dayjs().minute(30).toISOString();
-json.checkin_time = date;
+
+const now = dayjs();
+const future = now.subtract(30, 'm');
+
+json.checkin_time = future.toISOString();
 fs.writeFileSync('../mockdata.json', JSON.stringify(json));
+
